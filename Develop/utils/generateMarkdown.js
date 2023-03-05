a624d338c063da961e73e500d46267f0b8c683fd
Jimmy Yin <118097335+xKranze@users.noreply.github.com>
2023-03-04 18:15:08 -0800

    finished and tested

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,10 +1,13 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if(!license || license === "" || license === "None"){
+  if(license === "Apache License 2.0"){
+    return `![License](https://img.shields.io/badge/license-Apache-blue)`;
+  }else if(license === "MIT License"){
+    return `![License](https://img.shields.io/badge/license-MIT-green)`;
+  }else{
     return "";
   }
-  return `![License](https://img.shields.io/badge/license-${license}-green.svg)`;
 }
 
 // TODO: Create a function that returns the license link
@@ -14,19 +17,21 @@ function renderLicenseLink(license) {
     return "https://www.apache.org/licenses/LICENSE-2.0.txt";
   }else if(license === "MIT License"){
     return "https://mit-license.org/";
+  }else{
+    return "";
   }
-  return "";
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if(!license || license === "" || license === "None"){
-    return "";
+  if(license === "Apache License 2.0"){
+    return `This project is licensed under the Apache 2.0 license.`;
+  }else if(license === "MIT License"){
+    return `This project is licensed under the MIT license.`;
+  }else{
+    return "This project is not licensed in any way.";
   }
-  return `## License
-    
-  This project is licensed under the ${license} license.`;
 }
 
 // TODO: Create a function to generate markdown for README
@@ -64,6 +69,7 @@ ${data.credits}
 ## License
 
 ${renderLicenseSection(data.license)}
+
 ${renderLicenseLink(data.license)}
 
 ## Badges
@@ -86,7 +92,7 @@ ${data.test}
 
 If you have any questions or concerns please contact me here: 
 
-- Github: ${data.github}
+- Github: https://github.com/${data.github}
 - Email: ${data.email}
 `;
 }
